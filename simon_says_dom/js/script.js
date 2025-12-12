83405d7e5c662d7d87a4495f27cfead9268b626c
Valeria Gabrielli <gabvale53@gmail.com>
2025-12-12 16:53:29 +0100

    primo tentativo di logica

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,23 @@
+/* Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+ */
+
+//creo un generatore casuale di 5 numeri con array 
+    //li faccio visualizzare in pagina
+
+//genero un countdown di 30 secondi
+//seleziono l'elemento di output
+//inserisco una variabile di inizio conteggio
+//lo faccio partire appena appaiono i numeri in pagina
+//ogni secondo mi toglie un'unità (10,9,8 ecc)
+//inserisco output con i secondi aggiornati 
+//gestisco l'aggiornamento dell'output
+//SE il countdoun finisce allora  
+//tolgo il display alla frase "Memorizza i numeri entro il tempo limite"
+    //richiamo in pagina il form che fino a questo punto non era visibile
+    
+
+    //all'invio del form il risultato mi dice quanti numeri ho azzeccato 
+
+
+
